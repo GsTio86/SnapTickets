6be4842d4c25ac8dccb2d8c459e87cba8621f729
gstio86 <bailung20000218@gmail.com>
2024-11-13 04:13:18 +0800

    調整SecurityConfig並支援前端正常串接

--- src/main/java/me/gt/snaptickets/config/SecurityConfig.java
+++ src/main/java/me/gt/snaptickets/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package me.gt.snaptickets.config;
 
+import me.gt.snaptickets.filter.AdminJwtAuthenticationFilter;
 import me.gt.snaptickets.filter.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -19,19 +24,61 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public AdminJwtAuthenticationFilter adminJwtAuthenticationFilter() {
+        return new AdminJwtAuthenticationFilter();
+    }
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain adminSecurityFilterChain(HttpSecurity http) throws Exception {
         return http
+                .securityMatcher("/admin/**")
                 .csrf(csrf -> csrf.disable())
                 .cors(cors -> {})
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/swagger-ui/**", "/api-docs/**", "/auth/*","/ticket/**", "/payment/**", "/order/**").permitAll() // 允許匿名訪問 /register
-                        .requestMatchers("/api/**", "/order/checkout", "/user/**").authenticated() // 其他 /api 路徑需要認證
-                        .anyRequest().permitAll()
-                )
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/admin/**").authenticated())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(adminJwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    @Order(2)
+    public SecurityFilterChain userSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .securityMatcher("/user/**", "/api/**", "/order/**")
+                .csrf(csrf -> csrf.disable())
+                .cors(cors -> {})
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/user/**", "/api/**", "/order/**").authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .httpBasic(httpBasic -> {})
                 .build();
+    }
+
+    @Bean
+    @Order(3)
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> {
+            auth.requestMatchers( "/","/swagger-ui/**", "/api-docs/**", "/auth/**",
+                             "/ticket/info/*","/ticket/all", "/ticket/*/image.png", "/ticket/*/*/image.png",
+                    "/payment/client", "/payment/result")
+                    .permitAll();
+        }).sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)).build();
+    }
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOriginPattern("*");
+        config.addAllowedMethod("*");
+        config.addAllowedHeader("*");
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
